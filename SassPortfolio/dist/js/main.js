571936116120ee68dc60689bbd77c0e0caa39c50
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-20 23:57:06 +0200

    Finishing style and JS

--- SassPortfolio/dist/js/main.js
+++ SassPortfolio/dist/js/main.js
@@ -1,5 +1,8 @@
 const menuButton = document.querySelector('.menu-btn');
 const burger = document.querySelector('.menu-btn_burger')
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-item');
 
 let showMenu = false;
 
@@ -8,9 +11,21 @@ menuButton.addEventListener('click', toggleMenu);
 function toggleMenu(){
     if(!showMenu){
         burger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        
+        navItems.forEach( (item) => {
+            item.classList.add('open');
+        })
         showMenu = true;
     }else{
         burger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+
+        navItems.forEach( (item) => {
+            item.classList.remove('open');
+        })
         showMenu = false;
     }
 }
