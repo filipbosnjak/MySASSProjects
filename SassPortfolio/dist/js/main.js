7c07a017ce82c2f7acdf1630ac14976fa2dccbb5
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-20 12:15:16 +0200

    Updating JS and adding some styles

--- SassPortfolio/dist/js/main.js
+++ SassPortfolio/dist/js/main.js
@@ -1,1 +1,16 @@
 const menuButton = document.querySelector('.menu-btn');
+const burger = document.querySelector('.menu-btn_burger')
+
+let showMenu = false;
+
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        burger.classList.add('open');
+        showMenu = true;
+    }else{
+        burger.classList.remove('open');
+        showMenu = false;
+    }
+}
